71b2783ab76112e71ca81138fe3049749d19a9a3
Alex Mehr <alirezaalipouri@gmail.com>
2018-05-29 01:49:30 -0700

    in process

---
+++ assets/app.js
@@ -0,0 +1,33 @@
+$("button").on("click", function () {
+    var sport = $(this).attr("data-sport");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        sport + "&api_key=dc6zaTOxFJmzC&limit=10";
+// $("#find-sport").on("click", function (event) {
+//     event.preventDefault();
+//   var sport = $("#sport-input").val();
+
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div class='item'>");
+
+                var rating = results[i].rating;
+
+                var p = $("<p>").text("Rating: " + rating);
+
+                var sportImage = $("<img>");
+                sportImage.attr("src", results[i].images.fixed_height.url);
+
+                gifDiv.prepend(p);
+                gifDiv.prepend(sportImage);
+
+                $("#gifs-appear-here").prepend(gifDiv);
+            }
+        });
+});
